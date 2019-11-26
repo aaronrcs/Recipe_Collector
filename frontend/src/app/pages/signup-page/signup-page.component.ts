948020d1073ad87e54133ca02fecb2280c3076c5
aaronrcs <aromer61@calstatela.edu>
2019-11-26 01:59:50 -0800

    Once the user successfully makes an account, the user gets redirected to login page. Need to add more validation

--- frontend/src/app/pages/signup-page/signup-page.component.ts
+++ frontend/src/app/pages/signup-page/signup-page.component.ts
@@ -1,6 +1,7 @@
 import { AuthService } from './../../auth.service';
 import { Component, OnInit } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,14 +11,18 @@ import { HttpResponse } from '@angular/common/http';
 })
 export class SignupPageComponent implements OnInit {
 
-  constructor( private authService: AuthService) { }
+  constructor( private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   onSignupClicked(email: string, password: string){
     this.authService.signup(email, password).subscribe((res: HttpResponse<any>) => {
-      
+      if(res.status === 200){
+        // Were able to make an account
+        // Re-direct user to login page
+        this.router.navigate(['/login']);
+      }
     })
   }
 
